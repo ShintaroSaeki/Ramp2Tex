a3c01d2b2dc3270a4b1d4af7d3b2ea4dba63d58e
ShintaroSaeki <63334692+ShintaroSaeki@users.noreply.github.com>
2021-01-23 19:10:36 +0900

    Completion

---
+++ Ramp2Tex/Assets/Scripts/MaterialPropertyController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshRenderer))]
+public class MaterialPropertyController : MonoBehaviour
+{
+    [SerializeField] [Range(0, 1)] protected float _RampAmount;
+    [SerializeField] [Range(0, 1)] protected float _DissolveAmount;
+
+    private MeshRenderer _MeshRenderer;
+    private MaterialPropertyBlock _MaterialPropertyBlock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _MeshRenderer = this.GetComponent<MeshRenderer>();
+        _MaterialPropertyBlock = new MaterialPropertyBlock();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _MeshRenderer.GetPropertyBlock(_MaterialPropertyBlock);
+
+        _MaterialPropertyBlock.SetFloat("_RampAmount", _RampAmount);
+        _MaterialPropertyBlock.SetFloat("_DissolveAmount", _DissolveAmount);
+
+        _MeshRenderer.SetPropertyBlock(_MaterialPropertyBlock);
+    }
+}
